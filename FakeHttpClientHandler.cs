4f511bab8e0ca223ca931af7e80812ddacdbe8ff
Arul <33111213+sarul84@users.noreply.github.com>
2019-02-08 02:15:20 +0530

    Add files via upload

---
+++ FakeHttpClientHandler.cs
@@ -0,0 +1,41 @@
+﻿//----------------------------------------------------------------------------------
+// <copyright file="FakeHttpClientHandler.cs" company="Prakrishta Technologies">
+//     Copyright (c) 2019 Prakrishta Technologies. All rights reserved.
+// </copyright>
+// <author>Arul Sengottaiyan</author>
+// <date>2/8/2019</date>
+// <summary>Fake Http Client Handler</summary>
+//-----------------------------------------------------------------------------------
+
+namespace Prakrishta.Data.Test
+{
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Fake http client handler class
+    /// </summary>
+    public class FakeHttpClientHandler : DelegatingHandler
+    {
+        /// <summary>
+        /// Holds mock http response message
+        /// </summary>
+        private readonly HttpResponseMessage responseMessage;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeHttpClientHandler"/> class.
+        /// </summary>
+        /// <param name="httpResponseMessage">Fake response message</param>
+        public FakeHttpClientHandler(HttpResponseMessage httpResponseMessage)
+        {
+            this.responseMessage = httpResponseMessage;
+        }
+
+        /// <inheritdoc />
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(responseMessage);
+        }
+    }
+}
---
+++ FakeHttpClientResponse.cs
@@ -0,0 +1,53 @@
+﻿//----------------------------------------------------------------------------------
+// <copyright file="FakeHttpClientResponse.cs" company="Prakrishta Technologies">
+//     Copyright (c) 2019 Prakrishta Technologies. All rights reserved.
+// </copyright>
+// <author>Arul Sengottaiyan</author>
+// <date>2/8/2019</date>
+// <summary>Fake Http Client Response Test</summary>
+//-----------------------------------------------------------------------------------
+
+namespace Prakrishta.Data.Test
+{
+    using Newtonsoft.Json;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+
+    public class FakeHttpClientResponse
+    {
+        /// <summary>
+        /// Test method that mimicks fake http request and response
+        /// </summary>
+        public void MockHttpClientTest()
+        {
+            var httpClientResponse = new HttpResponseMessage()
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(this.YourMockData()), Encoding.UTF8, "application/json"),
+                StatusCode = HttpStatusCode.OK
+            };
+
+            var fakeHandler = new FakeHttpClientHandler(httpClientResponse);
+            var httpClient = new HttpClient(fakeHandler)
+            {
+                BaseAddress = new System.Uri("https://test.io")
+            };
+
+            var request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+            };
+
+            var response = httpClient.SendAsync(request);
+        }
+
+        /// <summary>
+        /// Write your mock data here and use appropriate return type
+        /// </summary>
+        /// <returns></returns>
+        private dynamic YourMockData()
+        {
+            return new object();
+        }
+    }
+}
